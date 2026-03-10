353ca227392c564d12d45c1e75941d5d5e3488f5
taleddie <eduardaalcsantos@gmail.com>
2026-03-09 22:38:08 -0300

    cuscuz

---
+++ scr/App.java
@@ -0,0 +1,6 @@
+/*public class App {
+    public static void main(String[] args) throws Exception {
+        usuario user = new usuario();
+    }
+}
+*/
---
+++ scr/Usuario.java
@@ -0,0 +1,17 @@
+public class Usuario {
+    int id;
+    String nome;
+
+    static int contador = 1;
+
+    public Usuario(String nome) {
+        this.id = contador++;
+        this.nome = nome;
+    }
+
+    public void mostrarUsuario() {
+        System.out.println("ID: " + this.id);
+        System.out.println("Nome: " + this.nome);
+    }
+    
+}
